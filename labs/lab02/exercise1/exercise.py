ee6bf721a63cff0456c936780dc38c2b16c7f6d6
allyne77 <allyneninie@gmail.com>
2025-12-08 12:05:05 +0800

    commit

--- labs/lab02/exercise1/exercise.py
+++ labs/lab02/exercise1/exercise.py
@@ -3,8 +3,12 @@
 
 def is_budget_sufficient(one_way_km, km_per_liter, price_per_liter, budget):
     # TODO: Implement this function
+
     # Calculate round trip cost and checks if within budget
-    pass
+    pass   
+
+
+
 
 # Test your code here
 print("Testing Road Trip Budgeter...")
---
+++ labs/lab02/exercise2.py
@@ -0,0 +1,15 @@
+# Student 1
+name1 = "Ahmad"
+marks1 = 85
+print(f"{name1} scored {marks1} marks")
+
+# Student 2
+name2 = "Fatimah"
+marks2 = 92
+print(f"{name2} scored {marks2} marks")
+
+def display_score(name, marks):  # Two parameters for two changing values
+    print(f"{name} scored {marks} marks")
+
+display_score("Ahmad", 85)
+display_score("Fatimah", 92)
