108eb78854af884cb6fa3dac373149af5e3bd685
allyne77 <allyneninie@gmail.com>
2026-04-02 10:07:37 +0800

    lab9

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -2,4 +2,19 @@ import pandas as pd
 
 
 def explore_data(filename):
-    pass
+    df = pd.read_csv(filename)
+    total_student = len(df)
+    subjects = ["Math","Science","English"]
+    math_avg = [(df["Math"]).mean() , 1]
+    highest_math_student = df.loc[df["Math"].max(), "Name"]
+
+    result = {
+
+        "total_students": total_student,
+        "subjects": subjects,
+        "math_average": math_average,
+        "highest_math_student": highest_math_student
+    }
+
+result = explore_data("labs/lab09/data/students.csv")
+print(result)
